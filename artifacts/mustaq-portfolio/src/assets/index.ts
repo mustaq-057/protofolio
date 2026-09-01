3b68b6ca1059cf0f0eceaeee339b6ecf4f31963b
Mustaq Ahmed <mustaqmustaqmustqq-cell@users.noreply.github.com>
2026-09-01 07:39:09 +0530

    updated asset exports and replaced logo and threejs icon

--- artifacts/mustaq-portfolio/src/assets/index.ts
+++ artifacts/mustaq-portfolio/src/assets/index.ts
@@ -6,12 +6,20 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import cognicode from "./cognicode.png";
+import finetuning from "./finetuning.png";
+import grova from "./grova.png";
+import sentiment from "./sentiment.png";
+import journal from "./journal.png";
+import lush from "./lush.png";
+import raiser from "./raiser.png";
+import profile from "./profile.jpg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
+import html from "./tech/html.svg";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
@@ -20,6 +28,13 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import jira from "./tech/jira.svg";
+import jenkins from "./tech/jenkins.svg";
+import express from "./tech/express.svg";
+import fastify from "./tech/fastify.svg";
+import bun from "./tech/bun.svg";
+import python from "./tech/python.svg";
+import ml from "./tech/ml.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -39,6 +54,14 @@ export {
   github,
   menu,
   close,
+  cognicode,
+  finetuning,
+  grova,
+  sentiment,
+  journal,
+  lush,
+  raiser,
+  profile,
   css,
   docker,
   figma,
@@ -52,6 +75,13 @@ export {
   tailwind,
   typescript,
   threejs,
+  jira,
+  jenkins,
+  express,
+  fastify,
+  bun,
+  python,
+  ml,
   meta,
   shopify,
   starbucks,
@@ -60,3 +90,8 @@ export {
   jobit,
   tripguide,
 };
+export { default as udemy_logo } from './udemy_logo.png';
+export { default as coursera_logo } from './coursera_logo.png';
+export { default as michigan_logo } from './michigan_logo.png';
+export { default as chicago_logo } from './chicago_logo.png';
+export { default as mindluster_logo } from './mindluster_logo.png';
